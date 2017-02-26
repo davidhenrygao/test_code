5c4cb340f67393c10d55a68d5e4562f7936e0c7b
davidhenrygao <davidhenrygao@gmail.com>
2017-02-27 01:34:07 +0800

    add some test

--- lua/coroutine/main.c
+++ lua/coroutine/main.c
@@ -6,8 +6,9 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <time.h>
+#include <signal.h>
 
-#include "lua.h"
+//#include "lua.h"
 
 typedef void* (ThreadRoutine)(void*);
 
@@ -19,12 +20,15 @@ typedef union Channel {
   };
 }Channel_t;
 
+#define MAX_ERR_BUF_LEN 128
+char err_buf[MAX_ERR_BUF_LEN];
+
 Channel_t g_rrch; //request read channel
 Channel_t g_rwch; //request write channel
 Channel_t g_srch; //service read channel
 Channel_t g_swch; //service write channel
 
-void OpenChannel(Channel_t);
+void OpenChannel(Channel_t *);
 void CreateThread(pthread_t *, ThreadRoutine *);
 
 ThreadRoutine ClientRountine;
@@ -39,14 +43,16 @@ int main(int argc, char *argv[]) {
 
   srand(time(NULL));
 
-  OpenChannel(g_rrch);
-  OpenChannel(g_rwch);
-  OpenChannel(g_srch);
-  OpenChannel(g_swch);
+  signal(SIGPIPE, SIG_IGN);
+
+  OpenChannel(&g_rrch);
+  OpenChannel(&g_rwch);
+  OpenChannel(&g_srch);
+  OpenChannel(&g_swch);
 
   CreateThread(&client_td, &ClientRountine);
-  //CreateThread(&server_td, &ServerRountine);
-  //CreateThread(&inner_server_td, &InnerServerRountine);
+  CreateThread(&server_td, &ServerRountine);
+  CreateThread(&inner_server_td, &InnerServerRountine);
 
   pthread_join(client_td, NULL);
   pthread_join(server_td, NULL);
@@ -55,22 +61,57 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-void OpenChannel(Channel_t ch) {
-  int ret = pipe(ch.fds);
+#define MAX_TRY_TIMES 10
+#define PIPE_ATOMIC_SIZE 4096
+
+int ReadPipe(int fd, void* buf, size_t len) {
+  int maxtry = MAX_TRY_TIMES;
+
+}
+
+int WritePipe(int fd, void* buf, size_t len) {
+  int maxtry = MAX_TRY_TIMES;
+  size_t total = 0;
+  size_t left = 0;
+  size_t wlen = 0;
+  ssize_t ret;
+  while (total < len) {
+    left = len - total;
+    wlen = left < PIPE_ATOMIC_SIZE ? left : PIPE_ATOMIC_SIZE;
+    ret = write(fd, buf+total, wlen);
+    if (ret == -1) {
+      if ((errno == EAGAIN || errno == EWOULDBLOCK) && (maxtry > 0)) {
+        maxtry--;
+        sleep(1);
+      } else {
+        printf("Write pipe error: %s.\n", 
+            strerror_r(errno, err_buf, MAX_ERR_BUF_LEN));
+        return ret;
+      }
+    }
+    total += wlen;
+  }
+  return 0;
+}
+
+void OpenChannel(Channel_t *pch) {
+  int ret = pipe(pch->fds);
   if (ret != 0) {
-    printf("Open pipe error: %s!\n", strerror(ret));
+    printf("Open pipe error: %s!\n", 
+        strerror_r(ret, err_buf, MAX_ERR_BUF_LEN));
     exit(-1);
   }
-  int flags = fcntl(ch.rfd, F_GETFL);
-  ret = fcntl(ch.rfd, F_SETFL, flags | O_NONBLOCK);
-  flags = fcntl(ch.wfd, F_GETFL);
-  ret = fcntl(ch.wfd, F_SETFL, flags | O_NONBLOCK);
+  int flags = fcntl(pch->rfd, F_GETFL);
+  ret = fcntl(pch->rfd, F_SETFL, flags | O_NONBLOCK);
+  flags = fcntl(pch->wfd, F_GETFL);
+  ret = fcntl(pch->wfd, F_SETFL, flags | O_NONBLOCK);
 }
 
 void CreateThread(pthread_t *ptid, ThreadRoutine *routine) {
   int ret = pthread_create(ptid, NULL, routine, NULL);
   if (ret != 0) {
-    printf("Create Thread failed: %s!\n", strerror(ret));
+    printf("Create Thread failed: %s!\n", 
+        strerror_r(ret, err_buf, MAX_ERR_BUF_LEN));
     exit(-1);
   }
 }
@@ -92,21 +133,21 @@ void* ClientRountine(void* arg) {
       if (errno == EAGAIN || errno == EWOULDBLOCK) {
         sleep(1);
       } else {
-        printf("ClientRountine write error: %s!\n", strerror(errno));
+        printf("ClientRountine write error: %s!\n", 
+            strerror_r(ret, err_buf, MAX_ERR_BUF_LEN));
         return NULL;
       }
     }
     wbs += ret;
   }
-  for (i = 0; i < 10; ++i) {
+  for (i = 0; i < 10; ) {
     ret = read(g_rrch.rfd, (void*)(buf+i), sizeof(int));
-    printf("ClientRountine read ret is %d.\n", ret);
     if (ret == -1) {
       if (errno == EAGAIN || errno == EWOULDBLOCK) {
-        printf("ClientRountine sleep 1s.\n");
         sleep(1);
       } else {
-        printf("ClientRountine read error: %s!\n", strerror(errno));
+        printf("ClientRountine read error: %s!\n", 
+            strerror_r(ret, err_buf, MAX_ERR_BUF_LEN));
         return NULL;
       }
     }
@@ -114,6 +155,9 @@ void* ClientRountine(void* arg) {
       printf("Request Read Channel's write end has been closed \
           when %d times read operation.\n", i);
       return NULL;
+    }
+    if (ret == sizeof(int)) {
+      ++i;
     }
   }
   printf("Return result: \n");
@@ -136,11 +180,11 @@ void* ServerRountine(void* arg) {
   while(max_idle > 0) {
     work_flag = 0;
     ret = read(g_rwch.rfd, (void*)&req, sizeof(req));
-    printf("ServerRountine read ret is %d.\n", ret);
+    //printf("ServerRountine read ret(%d), req(%d).\n", ret, req);
     if (ret == -1) {
       if (errno != EAGAIN || errno != EWOULDBLOCK) {
         printf("ServerRountine read request channel error: %s!\n", 
-            strerror(errno));
+            strerror_r(ret, err_buf, MAX_ERR_BUF_LEN));
         return NULL;
       }
     }
@@ -157,7 +201,7 @@ void* ServerRountine(void* arg) {
             sleep(1);
           } else {
             printf("ServerRountine write inner service channel error: %s!\n", 
-                strerror(errno));
+                strerror_r(ret, err_buf, MAX_ERR_BUF_LEN));
             return NULL;
           }
         }
@@ -171,7 +215,7 @@ void* ServerRountine(void* arg) {
     if (ret == -1) {
       if (errno != EAGAIN || errno != EWOULDBLOCK) {
         printf("ServerRountine read service channel error: %s!\n", 
-            strerror(errno));
+            strerror_r(ret, err_buf, MAX_ERR_BUF_LEN));
         return NULL;
       }
     }
@@ -188,7 +232,7 @@ void* ServerRountine(void* arg) {
             sleep(1);
           } else {
             printf("ServerRountine write request channel error: %s!\n", 
-                strerror(errno));
+                strerror_r(ret, err_buf, MAX_ERR_BUF_LEN));
             return NULL;
           }
         }
@@ -216,13 +260,16 @@ void* InnerServerRountine(void* arg) {
   int work_flag = 0;
   ssize_t ret, ret2;
   pthread_t tid;
+  int buf[10];
+  int pos = 0;
   while(max_idle > 0) {
     work_flag = 0;
     ret = read(g_swch.rfd, (void*)&req, sizeof(req));
+    //printf("InnerServerRountine read ret(%d), req(%d).\n", ret, req);
     if (ret == -1) {
       if (errno != EAGAIN || errno != EWOULDBLOCK) {
         printf("InnerServerRountine read service channel error: %s!\n", 
-            strerror(errno));
+            strerror_r(ret, err_buf, MAX_ERR_BUF_LEN));
         return NULL;
       }
     }
@@ -232,10 +279,12 @@ void* InnerServerRountine(void* arg) {
     }
     if (ret == sizeof(req)) {
       work_flag = 1;
-      ret2 = pthread_create(&tid, NULL, &WorkRoutine, (void*)&req);
+      buf[pos] = req;
+      ret2 = pthread_create(&tid, NULL, &WorkRoutine, (void*)(buf+pos));
+      pos++;
       if (ret2 != 0) {
         printf("InnerServerRountine create work thread error: %s.\n", 
-            strerror(errno));
+            strerror_r(ret, err_buf, MAX_ERR_BUF_LEN));
         while (1) {
           ret2 = write(g_srch.wfd, (void*)&req, sizeof(req));
           if (ret2 == -1) {
@@ -243,7 +292,7 @@ void* InnerServerRountine(void* arg) {
               sleep(1);
             } else {
               printf("InnerServerRountine write inner service channel error: \
-                  %s!\n", strerror(errno));
+                  %s!\n", strerror_r(ret, err_buf, MAX_ERR_BUF_LEN));
               return NULL;
             }
           }
@@ -251,6 +300,8 @@ void* InnerServerRountine(void* arg) {
             break;
           }
         }
+      } else {
+        pthread_detach(tid);
       }
     }
 
@@ -268,6 +319,7 @@ void* InnerServerRountine(void* arg) {
 void* WorkRoutine(void* arg) {
   long r = rand() % 10 + 1;
   int ret;
+  printf("Request %d sleep %ld seconds.\n", *(int*)arg, r);
   sleep((unsigned int)r);
   while (1) {
     ret = write(g_srch.wfd, arg, sizeof(int));
@@ -276,7 +328,7 @@ void* WorkRoutine(void* arg) {
         sleep(1);
       } else {
         printf("InnerServerRountine write inner service channel error: \
-            %s!\n", strerror(errno));
+            %s!\n", strerror_r(ret, err_buf, MAX_ERR_BUF_LEN));
         return NULL;
       }
     }
